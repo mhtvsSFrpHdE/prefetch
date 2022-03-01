a1d1d4235c7a47d80662119f3a8a055b15c6f130
mhtvsSFrpHdE <2bemperor47@gmail.com>
2022-03-01 18:09:52 +0800

    Setting api getArray

--- qpp/prefetch/Source/Setting/setting.cpp
+++ qpp/prefetch/Source/Setting/setting.cpp
@@ -10,3 +10,24 @@ void Setting::init()
     // Read ini from exe stored folder
     setting = new QSettings(QCoreApplication::applicationDirPath() + "/prefetch.ini", QSettings::IniFormat);
 }
+
+QStringList Setting::getArray(const QString &groupName, QSettings *qSettings)
+{
+    QStringList values;
+
+    qSettings->beginGroup(groupName);
+
+    auto keysInGroup = qSettings->childKeys();
+    for (int i = 0; i < keysInGroup.size(); ++i)
+    {
+
+        auto key = keysInGroup[i];
+        auto value = qSettings->value(key).toString();
+
+        values << value;
+    };
+
+    qSettings->endGroup();
+
+    return values;
+}
--- qpp/prefetch/Source/Setting/setting.h
+++ qpp/prefetch/Source/Setting/setting.h
@@ -9,6 +9,19 @@ public:
     // Any init code
     static void init();
 
+    // Give a setting group name, return all values under that group
+    // Use case: retrieve user input array
+    //
+    // Example:
+    //     setting.ini
+    //         [Array]
+    //         value1=1
+    //         value2=2
+    //         value3=3
+    //     auto myArray = getArray("Array");
+    //         myArray content: {1,2,3}
+    static QStringList getArray(const QString &groupName, QSettings *qSettings);
+
 private:
     // Disallow creating an instance of this object
     Setting() {}
