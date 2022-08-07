ef37a6da487981ffb47474d4dc9c2ae54c0997ae
mhtvsSFrpHdE <2bemperor47@gmail.com>
2022-08-08 00:45:13 +0800

    Resume command

--- qpp/prefetch/Source/Input/Thread/loop_thread.cpp
+++ qpp/prefetch/Source/Input/Thread/loop_thread.cpp
@@ -4,6 +4,7 @@
 #include "..\..\Input\stdin.h"
 #include "..\..\Output\stdout.h"
 #include "..\..\Core\Thread\read_thread.h"
+#include "..\..\Core\read_file.h"
 
 LoopThread::LoopThread() {}
 
@@ -39,11 +40,29 @@ namespace ConsoleCommandFunction
         StdOut::consoleOutput->flush();
 
         ReadThread::pause = true;
+
+        *StdOut::consoleOutput << "Pause has requested, good luck"
+                               << endl;
+        StdOut::consoleOutput->flush();
     };
+
+    void resume()
+    {
+        *StdOut::consoleOutput << "Trying to resume prefetch"
+                               << endl;
+        StdOut::consoleOutput->flush();
+
+        ReadThread::pause = false;
+        if (ReadFile::sleepThreadAddress->isRunning())
+        {
+            ReadFile::sleepThreadAddress->terminate();
+        }
+    }
 }
 
 // Cool stuff: https://stackoverflow.com/questions/8157625/how-do-i-populate-values-of-a-static-qmap-in-c-qt
 // Use initializer list and one of the QMap constructor
 QMap<QString, void (*)()> LoopThread::commandMap(
     std::map<QString, void (*)()>{
-        {"pause", &ConsoleCommandFunction::pause}});
+        {"pause", &ConsoleCommandFunction::pause},
+        {"resume", &ConsoleCommandFunction::resume}});
