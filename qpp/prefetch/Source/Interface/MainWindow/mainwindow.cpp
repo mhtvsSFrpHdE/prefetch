172e51014d8ddd47b3e0f0258ab5a5f1289683be
mhtvsSFrpHdE <2bemperor47@gmail.com>
2022-08-28 02:51:21 +0800

    Fix character print order may go wrong

--- qpp/prefetch/Source/Interface/MainWindow/mainwindow.cpp
+++ qpp/prefetch/Source/Interface/MainWindow/mainwindow.cpp
@@ -109,6 +109,12 @@ void MainWindow::start()
 
 void MainWindow::print_slot(QString textToPrint)
 {
+    // Make sure text is inserted after last character
+    // User may copy text to clipboard, so cursor position is changed
+    auto cursor = ui->stdOut_plainTextEdit->textCursor();
+    cursor.movePosition(QTextCursor::End);
+    ui->stdOut_plainTextEdit->setTextCursor(cursor);
+
     ui->stdOut_plainTextEdit->insertPlainText(textToPrint);
 }
 
