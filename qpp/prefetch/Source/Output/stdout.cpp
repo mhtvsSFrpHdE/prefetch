f3157265ba8c28e8c6dadcc90710b2974f830538
mhtvsSFrpHdE <2bemperor47@gmail.com>
2022-08-27 08:40:22 +0800

    New interface send print to multiple target

--- qpp/prefetch/Source/Output/stdout.cpp
+++ qpp/prefetch/Source/Output/stdout.cpp
@@ -1,8 +1,35 @@
 #include "stdout.h"
+#include "..\Global\global.h"
 
 QTextStream *StdOut::consoleOutput;
 
 void StdOut::init()
 {
     consoleOutput = new QTextStream(stdout);
+}
+
+void StdOut::print(QString textToPrint)
+{
+    *consoleOutput << textToPrint;
+
+    Global::qMainWindow->StdOut_print(textToPrint);
+}
+
+void StdOut::printLine(QString textToPrint)
+{
+    *consoleOutput << textToPrint
+                   << endl;
+    flush();
+
+    Global::qMainWindow->StdOut_printLine(textToPrint);
+}
+
+void StdOut::printEndl()
+{
+    *consoleOutput << endl;
+}
+
+void StdOut::flush()
+{
+    consoleOutput->flush();
 }
--- qpp/prefetch/Source/Output/stdout.h
+++ qpp/prefetch/Source/Output/stdout.h
@@ -9,6 +9,18 @@ public:
     // Any init code
     static void init();
 
+    // Print
+    static void print(QString textToPrint);
+
+    // Print line (append endl after text)
+    static void printLine(QString textToPrint);
+
+    // Print endl
+    static void printEndl();
+
+    // Without flush printed text may not appear in console immediately
+    static void flush();
+
 private:
     // Disallow creating an instance of this object
     StdOut() {}
