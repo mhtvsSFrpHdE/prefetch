284754b1832a14f4df108d0628aee7007eaab6eb
mhtvsSFrpHdE <2bemperor47@gmail.com>
2022-09-14 22:56:10 +0800

    Define guard

--- qpp/prefetch/Source/Define/define.h
+++ qpp/prefetch/Source/Define/define.h
@@ -9,18 +9,28 @@
 // Therefore, the console will only use for debug purpose
 // Compile with `CONFIG += console` and `CONSOLE_ENABLED true` to enable console
 // Otherwize for a user release, keep `CONSOLE_ENABLED false`
+#ifndef CONSOLE_ENABLED
 #define CONSOLE_ENABLED false
+#endif
 
 // Add "Test" to tray menu, a handy place to execute some random fancy stuff
+#ifndef TEST_TRAY_MENU_ENABLED
 #define TEST_TRAY_MENU_ENABLED false
+#endif
 
 // Load and run log
 //     When set to true, print to stdout also sync to log file
+#ifndef LOG_ENABLED
 #define LOG_ENABLED true
+#endif
 
 // Try to report last known position to log
 //     This function designed to find crash in release (usually no gdb attached)
+#ifndef LAST_KNOWN_POSITION_ENABLED
 #define LAST_KNOWN_POSITION_ENABLED true
+#endif
 
 // Misc
+#ifndef SKIP_STARTUP_ITEM
 #define SKIP_STARTUP_ITEM false
+#endif
