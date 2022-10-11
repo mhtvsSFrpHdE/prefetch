3b346d3388f53dc9eb20864b682d26a072943959
mhtvsSFrpHdE <2bemperor47@gmail.com>
2022-10-11 13:44:43 +0800

    Fix compile error

--- qpp/prefetch/Source/Translate/translate_tool.cpp
+++ qpp/prefetch/Source/Translate/translate_tool.cpp
@@ -1,5 +1,7 @@
-#include "translate_tool.h"
+// Redefining tr macro, any other include go first
 #include "..\Global\global.h"
+
+#include "translate_tool.h"
 
 QString TranslateTool::_tr(const char *className, const char *text)
 {
