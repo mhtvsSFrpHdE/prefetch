068e0c06a1370b5c4ff43d1c663357b3669bc24e
mhtvsSFrpHdE <2bemperor47@gmail.com>
2022-10-11 14:04:12 +0800

    Update Google translate

--- qpp/prefetch/Source/Translate/File/prefetch_en_US.ts
+++ qpp/prefetch/Source/Translate/File/prefetch_en_US.ts
@@ -4,102 +4,114 @@
 <context>
     <name>Const_Core::Message</name>
     <message>
-        <location filename="../../Core/const_core.cpp" line="23"/>
+        <location filename="../../Core/const_core.cpp" line="40"/>
         <source>Scan folder...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../Core/const_core.cpp" line="24"/>
+        <location filename="../../Core/const_core.cpp" line="41"/>
         <source>Cache found, restore file list</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../Core/const_core.cpp" line="25"/>
+        <location filename="../../Core/const_core.cpp" line="42"/>
         <source>Prefetching...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../Core/const_core.cpp" line="26"/>
+        <location filename="../../Core/const_core.cpp" line="43"/>
         <source>Idle, Time: </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../Core/const_core.cpp" line="27"/>
+        <location filename="../../Core/const_core.cpp" line="44"/>
         <source> Sec</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../Core/const_core.cpp" line="28"/>
+        <location filename="../../Core/const_core.cpp" line="45"/>
         <source>Scan complete, Time: </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../Core/const_core.cpp" line="30"/>
+        <location filename="../../Core/const_core.cpp" line="47"/>
         <source>ss.zzz</source>
-        <translatorcomment>Rarely necessary to modify these texts</translatorcomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../Core/const_core.cpp" line="31"/>
+        <location filename="../../Core/const_core.cpp" line="48"/>
         <source>Rescan interval reached.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../Core/const_core.cpp" line="32"/>
+        <location filename="../../Core/const_core.cpp" line="49"/>
         <source>Cache will expire on next run.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Core/const_core.cpp" line="50"/>
+        <source>Run startup items...</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
-    <name>Const_Global::CommonString</name>
+    <name>Const_Input::Message</name>
     <message>
-        <location filename="../../Global/const_global.cpp" line="17"/>
-        <source> </source>
+        <location filename="../../Input/const_input.cpp" line="40"/>
+        <source>Invalid command</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Input/const_input.cpp" line="41"/>
+        <source>Trying to pause prefetch</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Input/const_input.cpp" line="42"/>
+        <source>Pause has requested, good luck</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Input/const_input.cpp" line="43"/>
+        <source>Trying to resume prefetch</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Input/const_input.cpp" line="44"/>
+        <source>Trying to exit prefetch</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Input/const_input.cpp" line="45"/>
+        <source>This can take a while in some case</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Input/const_input.cpp" line="46"/>
+        <source>This function contain test code</source>
         <translatorcomment>Rarely necessary to modify these texts</translatorcomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../Global/const_global.cpp" line="18"/>
-        <source>
-</source>
-        <translatorcomment>Rarely necessary to modify these texts</translatorcomment>
+        <location filename="../../Input/const_input.cpp" line="47"/>
+        <source>Trying to run program</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../Global/const_global.cpp" line="19"/>
-        <source>    </source>
-        <translatorcomment>Rarely necessary to modify these texts</translatorcomment>
+        <location filename="../../Input/const_input.cpp" line="49"/>
+        <source>Trying to expire scan cache</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../Global/const_global.cpp" line="20"/>
-        <source>&quot;</source>
-        <translatorcomment>Rarely necessary to modify these texts</translatorcomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../Global/const_global.cpp" line="21"/>
-        <source>/</source>
-        <translatorcomment>Rarely necessary to modify these texts</translatorcomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../Global/const_global.cpp" line="22"/>
-        <source>&lt;default&gt;</source>
-        <translatorcomment>Rarely necessary to modify these texts</translatorcomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../Global/const_global.cpp" line="23"/>
-        <source>,</source>
-        <translatorcomment>Rarely necessary to modify these texts</translatorcomment>
+        <location filename="../../Input/const_input.cpp" line="50"/>
+        <source>Scan cache not found, ignore</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>Const_MainWindow::ButtonText</name>
     <message>
-        <location filename="../../Interface/MainWindow/const_mainwindow.cpp" line="9"/>
+        <location filename="../../Interface/MainWindow/const_mainwindow.cpp" line="13"/>
         <source>Send</source>
         <translation type="unfinished"></translation>
     </message>
--- qpp/prefetch/Source/Translate/File/prefetch_zh_CN.ts
+++ qpp/prefetch/Source/Translate/File/prefetch_zh_CN.ts
@@ -4,102 +4,114 @@
 <context>
     <name>Const_Core::Message</name>
     <message>
-        <location filename="../../Core/const_core.cpp" line="23"/>
+        <location filename="../../Core/const_core.cpp" line="40"/>
         <source>Scan folder...</source>
-        <translation>清点可缓存文件...</translation>
+        <translation>清点可缓存文件列表...</translation>
     </message>
     <message>
-        <location filename="../../Core/const_core.cpp" line="24"/>
+        <location filename="../../Core/const_core.cpp" line="41"/>
         <source>Cache found, restore file list</source>
-        <translation>从缓存恢复文件列表</translation>
+        <translation>从缓存恢复可缓存文件列表</translation>
     </message>
     <message>
-        <location filename="../../Core/const_core.cpp" line="25"/>
+        <location filename="../../Core/const_core.cpp" line="42"/>
         <source>Prefetching...</source>
         <translation>缓存文件...</translation>
     </message>
     <message>
-        <location filename="../../Core/const_core.cpp" line="26"/>
+        <location filename="../../Core/const_core.cpp" line="43"/>
         <source>Idle, Time: </source>
         <translation>空闲，消耗时间：</translation>
     </message>
     <message>
-        <location filename="../../Core/const_core.cpp" line="27"/>
+        <location filename="../../Core/const_core.cpp" line="44"/>
         <source> Sec</source>
         <translation>秒</translation>
     </message>
     <message>
-        <location filename="../../Core/const_core.cpp" line="28"/>
+        <location filename="../../Core/const_core.cpp" line="45"/>
         <source>Scan complete, Time: </source>
         <translation>清点完成，时间：</translation>
     </message>
     <message>
-        <location filename="../../Core/const_core.cpp" line="30"/>
+        <location filename="../../Core/const_core.cpp" line="47"/>
         <source>ss.zzz</source>
-        <translatorcomment>Rarely necessary to modify these texts</translatorcomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../Core/const_core.cpp" line="31"/>
+        <location filename="../../Core/const_core.cpp" line="48"/>
         <source>Rescan interval reached.</source>
-        <translation>已符合重新清点条件，</translation>
+        <translation>已符合重新清点条件</translation>
     </message>
     <message>
-        <location filename="../../Core/const_core.cpp" line="32"/>
+        <location filename="../../Core/const_core.cpp" line="49"/>
         <source>Cache will expire on next run.</source>
-        <translation>缓存将在下次运行前删除。</translation>
+        <translation>可缓存文件列表将在下次运行前删除。</translation>
+    </message>
+    <message>
+        <location filename="../../Core/const_core.cpp" line="50"/>
+        <source>Run startup items...</source>
+        <translation>运行开机启动项...</translation>
     </message>
 </context>
 <context>
-    <name>Const_Global::CommonString</name>
+    <name>Const_Input::Message</name>
     <message>
-        <location filename="../../Global/const_global.cpp" line="17"/>
-        <source> </source>
+        <location filename="../../Input/const_input.cpp" line="40"/>
+        <source>Invalid command</source>
+        <translation>无效操作</translation>
+    </message>
+    <message>
+        <location filename="../../Input/const_input.cpp" line="41"/>
+        <source>Trying to pause prefetch</source>
+        <translation>尝试暂停正在进行的缓存</translation>
+    </message>
+    <message>
+        <location filename="../../Input/const_input.cpp" line="42"/>
+        <source>Pause has requested, good luck</source>
+        <translation>已发送暂停请求</translation>
+    </message>
+    <message>
+        <location filename="../../Input/const_input.cpp" line="43"/>
+        <source>Trying to resume prefetch</source>
+        <translation>尝试恢复缓存</translation>
+    </message>
+    <message>
+        <location filename="../../Input/const_input.cpp" line="44"/>
+        <source>Trying to exit prefetch</source>
+        <translation>尝试退出程序</translation>
+    </message>
+    <message>
+        <location filename="../../Input/const_input.cpp" line="45"/>
+        <source>This can take a while in some case</source>
+        <translation>有时需要一段时间</translation>
+    </message>
+    <message>
+        <location filename="../../Input/const_input.cpp" line="46"/>
+        <source>This function contain test code</source>
         <translatorcomment>Rarely necessary to modify these texts</translatorcomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../Global/const_global.cpp" line="18"/>
-        <source>
-</source>
-        <translatorcomment>Rarely necessary to modify these texts</translatorcomment>
-        <translation type="unfinished"></translation>
+        <location filename="../../Input/const_input.cpp" line="47"/>
+        <source>Trying to run program</source>
+        <translation>尝试运行其他程序</translation>
     </message>
     <message>
-        <location filename="../../Global/const_global.cpp" line="19"/>
-        <source>    </source>
-        <translatorcomment>Rarely necessary to modify these texts</translatorcomment>
-        <translation type="unfinished"></translation>
+        <location filename="../../Input/const_input.cpp" line="49"/>
+        <source>Trying to expire scan cache</source>
+        <translation>尝试删除已缓存的可缓存文件列表</translation>
     </message>
     <message>
-        <location filename="../../Global/const_global.cpp" line="20"/>
-        <source>&quot;</source>
-        <translatorcomment>Rarely necessary to modify these texts</translatorcomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../Global/const_global.cpp" line="21"/>
-        <source>/</source>
-        <translatorcomment>Rarely necessary to modify these texts</translatorcomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../Global/const_global.cpp" line="22"/>
-        <source>&lt;default&gt;</source>
-        <translatorcomment>Rarely necessary to modify these texts</translatorcomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../Global/const_global.cpp" line="23"/>
-        <source>,</source>
-        <translatorcomment>Rarely necessary to modify these texts</translatorcomment>
-        <translation type="unfinished"></translation>
+        <location filename="../../Input/const_input.cpp" line="50"/>
+        <source>Scan cache not found, ignore</source>
+        <translation>没有发现可缓存文件清单，忽略</translation>
     </message>
 </context>
 <context>
     <name>Const_MainWindow::ButtonText</name>
     <message>
-        <location filename="../../Interface/MainWindow/const_mainwindow.cpp" line="9"/>
+        <location filename="../../Interface/MainWindow/const_mainwindow.cpp" line="13"/>
         <source>Send</source>
         <translation>提交</translation>
     </message>
@@ -110,7 +122,7 @@
         <location filename="../../Interface/MainWindow/mainwindow.ui" line="14"/>
         <source>MainWindow</source>
         <translatorcomment>Rarely necessary to modify these texts</translatorcomment>
-        <translation></translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
