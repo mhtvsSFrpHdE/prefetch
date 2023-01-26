f64ebeb4496ba6e964201d526a9a85b4b308432f
mhtvsSFrpHdE <2bemperor47@gmail.com>
2023-01-26 22:20:09 +0800

    Self delete mutex example

--- qpp/prefetch/Source/Example/mutex_example.h
+++ qpp/prefetch/Source/Example/mutex_example.h
@@ -1,14 +1,39 @@
+#ifndef MutexExample_h
+#define MutexExample_h
+
 #include <QMutex>
 
 class MutexExample
 {
 public:
-    static QMutex *getLockedMutex();
+    // QMutex doesn't provide an accessible copy constructor
+    // https://stackoverflow.com/questions/22659453/is-this-an-issue-with-mutable-member-qmutex-as-a-private-member
+    static QMutex *getLockedMutex()
+    {
+        auto mutexAddress = new QMutex(QMutex::NonRecursive);
+        mutexAddress->lock();
+        return mutexAddress;
+    }
 };
 
-QMutex *MutexExample::getLockedMutex()
+// Create a QMutex object, delete it on it's lock function
+//     then delete QMutex manager itself
+class SelfDeleteMutexExample
 {
-    auto mutexAddress = new QMutex(QMutex::NonRecursive);
-    mutexAddress->lock();
-    return mutexAddress;
-}
+public:
+    SelfDeleteMutexExample()
+    {
+        mutex = MutexExample::getLockedMutex();
+    }
+    QMutex *mutex;
+
+    // Warning: remember to unlock in somewhere else, or it will deadlock!
+    void lockAndDeleteThis()
+    {
+        mutex->lock();
+        delete mutex;
+        delete this;
+    }
+};
+
+#endif
