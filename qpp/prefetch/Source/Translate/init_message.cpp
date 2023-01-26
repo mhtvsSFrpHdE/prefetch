4411826780b3a3cec31f5cc34a66c86250bce2fb
mhtvsSFrpHdE <2bemperor47@gmail.com>
2023-01-27 06:12:23 +0800

    Remove redundant include

--- qpp/prefetch/Source/Translate/init_message.cpp
+++ qpp/prefetch/Source/Translate/init_message.cpp
@@ -1,10 +1,7 @@
 #include "translator_loader.h"
 #include "..\Core\const_core.h"
-#include "..\Global\const_global.h"
 #include "..\Input\const_input.h"
 #include "..\Interface\MainWindow\const_mainwindow.h"
-#include "..\Output\const_log.h"
-#include "..\Setting\const_setting.h"
 #include "..\Interface\TrayIcon\const_tray_icon.h"
 
 void TranslatorLoader::initMessage()
