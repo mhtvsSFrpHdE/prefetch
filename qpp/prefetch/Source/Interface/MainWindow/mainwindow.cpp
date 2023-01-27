0c3aa5b7c252ecf8a9d7d63e2e1dc342700132a4
mhtvsSFrpHdE <2bemperor47@gmail.com>
2023-01-27 17:48:14 +0800

    Wrap different feature into code block

--- qpp/prefetch/Source/Interface/MainWindow/mainwindow.cpp
+++ qpp/prefetch/Source/Interface/MainWindow/mainwindow.cpp
@@ -24,81 +24,106 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent),
 
     // Initialize
 
-    startToTray = Setting::getBool(Instance, StartToTray, Setting::setting);
-    // Command line override
-    if (Global::commandLineArgumentAddress->getShowMainWindow())
+    // Start to tray
     {
-        startToTray = false;
+        startToTray = Setting::getBool(Instance, StartToTray, Setting::setting);
+
+        // Command line override
+        bool commandLineOverride = Global::commandLineArgumentAddress->getShowMainWindow();
+        if (commandLineOverride)
+        {
+            startToTray = false;
+        }
     }
 
-    minimizeToTray = Setting::getBool(Instance, MinimizeToTray, Setting::setting);
-
-    auto resizable = Setting::getBool(cgn_MainWindow, Resizable, Setting::setting);
-
-    auto getPrintOffset = Setting::getInt(Instance, PrintOffset, Setting::setting);
-    printOffset = getPrintOffset.result;
-
-    auto sizeArray = Setting::getStringList(cgn_MainWindow, Size, Setting::setting);
-    auto sizeWidth = sizeArray[0].toInt();
-    auto sizeHeight = sizeArray[1].toInt();
-    if (resizable)
+    // Minimize to try
     {
-        this->setMinimumSize(sizeWidth, sizeHeight);
-    }
-    else
-    {
-        this->setFixedSize(sizeWidth, sizeHeight);
+        minimizeToTray = Setting::getBool(Instance, MinimizeToTray, Setting::setting);
     }
 
-    // If value was given, update position
-    auto getPosition = Setting::getString(cgn_MainWindow, Position, Setting::setting);
-    if (getPosition != MainWindow_Position_Value::Default)
+    // Resizeable
     {
-        auto positionArray = Setting::getStringList(cgn_MainWindow, Position, Setting::setting);
-        this->move(positionArray[0].toInt(), positionArray[1].toInt());
+        auto resizable = Setting::getBool(cgn_MainWindow, Resizable, Setting::setting);
+        auto sizeArray = Setting::getStringList(cgn_MainWindow, Size, Setting::setting);
+        auto sizeWidth = sizeArray[0].toInt();
+        auto sizeHeight = sizeArray[1].toInt();
+
+        if (resizable)
+        {
+            this->setMinimumSize(sizeWidth, sizeHeight);
+        }
+        else
+        {
+            this->setFixedSize(sizeWidth, sizeHeight);
+        }
     }
 
-    lastKnownLine = EmptyString;
+    // Print offset
+    {
+        auto getPrintOffset = Setting::getInt(Instance, PrintOffset, Setting::setting);
+        printOffset = getPrintOffset.result;
+    }
+
+    // Position
+    {
+
+        // If value was given, update position
+        auto getPosition = Setting::getString(cgn_MainWindow, Position, Setting::setting);
+        if (getPosition != MainWindow_Position_Value::Default)
+        {
+            auto positionArray = Setting::getStringList(cgn_MainWindow, Position, Setting::setting);
+            this->move(positionArray[0].toInt(), positionArray[1].toInt());
+        }
+    }
+
+    // Last known line
+    {
+        lastKnownLine = EmptyString;
+    }
 
     // Zoom
-    Dpi::scale_qMainWindow(this);
-    Dpi::scale_qWidgetRect(ui->stdOut_plainTextEdit);
-    Dpi::scale_qWidgetRect(ui->command_lineEdit);
-    Dpi::scale_qWidgetRect(ui->sendCommand_pushButton);
+    {
+        Dpi::scale_qMainWindow(this);
+        Dpi::scale_qWidgetRect(ui->stdOut_plainTextEdit);
+        Dpi::scale_qWidgetRect(ui->command_lineEdit);
+        Dpi::scale_qWidgetRect(ui->sendCommand_pushButton);
+    }
 
     // Log size limit to prevent memory leak
-    auto getMaximumBlockCount = Setting::getInt(Instance, MaximumBlockCount, Setting::setting);
-    ui->stdOut_plainTextEdit->setMaximumBlockCount(getMaximumBlockCount.result);
+    {
+        auto getMaximumBlockCount = Setting::getInt(Instance, MaximumBlockCount, Setting::setting);
+        ui->stdOut_plainTextEdit->setMaximumBlockCount(getMaximumBlockCount.result);
+    }
 
     // Event
+    {
 
-    // StdOut print
-    connect(this, SIGNAL(print_signal(QString)), this, SLOT(print_slot(QString)));
+        // StdOut print
+        connect(this, SIGNAL(print_signal(QString)), this, SLOT(print_slot(QString)));
 
-    // Scroll bar to bottom on text change (after printed)
-    connect(ui->stdOut_plainTextEdit, SIGNAL(textChanged()), this, SLOT(scrollBarToBottom_slot()));
+        // Scroll bar to bottom on text change (after printed)
+        connect(ui->stdOut_plainTextEdit, SIGNAL(textChanged()), this, SLOT(scrollBarToBottom_slot()));
 
-    // StdIn send command
-
-    // Send button
-
-    // Action
-
-    // Click
-    connect(ui->sendCommand_pushButton, SIGNAL(clicked()), this, SLOT(sendCommand_action_slot()));
-    // Enter on command edit
-    connect(ui->command_lineEdit, SIGNAL(returnPressed()), this, SLOT(sendCommand_action_slot()));
+        // Send button
+        {
+            // Click
+            connect(ui->sendCommand_pushButton, SIGNAL(clicked()), this, SLOT(sendCommand_action_slot()));
+            // Enter on command edit
+            connect(ui->command_lineEdit, SIGNAL(returnPressed()), this, SLOT(sendCommand_action_slot()));
+        }
+    }
 
     // Interface text
+    {
+        // Get instance name
+        auto instanceName = Setting::getString(Instance, Name, Setting::setting);
 
-    // Get instance name
-    auto instanceName = Setting::getString(Instance, Name, Setting::setting);
+        // MainWindow title
+        setWindowTitle(instanceName);
 
-    // MainWindow title
-    setWindowTitle(instanceName);
-
-    // Send command button text
-    ui->sendCommand_pushButton->setText(SendCommand_PushButton);
+        // Send command button text
+        ui->sendCommand_pushButton->setText(SendCommand_PushButton);
+    }
 }
 
 MainWindow::~MainWindow()
