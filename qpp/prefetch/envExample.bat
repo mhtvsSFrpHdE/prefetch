7ec2d8798f588289815f018c4b67d0ad1c8ee2d4
mhtvsSFrpHdE <2bemperor47@gmail.com>
2023-03-27 13:18:13 +0800

    Clangd for VSCode

--- qpp/prefetch/envExample.bat
+++ qpp/prefetch/envExample.bat
@@ -3,4 +3,5 @@ set qtProject=prefetch.pro
 
 set qtPath=C:\Qt\4.8.7\bin
 set mingwPath=C:\i686-4.8.2-release-posix-dwarf-rt_v3-rev3\mingw32\bin
+set clangdPath=C:\Qt\Tools\QtCreator\bin\clang\bin
 set vsCodePath=C:\Microsoft VS Code\Code.exe
